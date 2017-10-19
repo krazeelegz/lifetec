3eb72736faf56f729189c41b16324c971dd48ac5
Krazeelegz <krish.jolliffe@gmail.com>
2017-10-18 17:18:01 -0700

    steel page done5

--- config/routes.rb
+++ config/routes.rb
@@ -9,23 +9,25 @@ Rails.application.routes.draw do
 
   get 'steel', to: 'application#steel'
 
-  get 'plarr_homes', to: 'application#plarr_homes'
+  # get 'plarr_homes', to: 'application#plarr_homes'
 
   get 'people', to: 'application#people'
+
+  get 'faq', to: 'application#faq'
 
   # get 'story', to: 'application#story'
 
   # get 'services', to: 'application#services'
 
-  get 'partners', to: 'application#partners'
+  # get 'partners', to: 'application#partners'
 
   get 'contact', to: 'application#contact'
 
-  get 'plarr', to: 'application#plarr'
+  # get 'plarr', to: 'application#plarr'
 
-  get 'smart_homes', to: 'application#smart_homes'
+  # get 'smart_homes', to: 'application#smart_homes'
 
-  get 'sustainable_homes', to: 'application#sustainable_homes'
+  # get 'sustainable_homes', to: 'application#sustainable_homes'
 
 
 end
