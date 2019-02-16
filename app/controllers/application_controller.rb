3bfcde05401928abe01cec3e82344a8a37d3517d
krazeelegz <krazeelegz@users.noreply.github.com>
2019-02-16 13:17:24 -0800

    force_ssl (#1)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  force_ssl if: :ssl_configured?
+
+  def ssl_configured?
+    Rails.env.production?
+  end
 
   def home
   end
